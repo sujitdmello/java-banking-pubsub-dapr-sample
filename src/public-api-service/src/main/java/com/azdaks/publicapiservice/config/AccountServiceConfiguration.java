273f99439ca076bc3617118a52cfbfaa068a6e60
Mahmut Canga <cangamahmut@gmail.com>
2023-05-26 12:31:41 +0100

    fix autowiring issues

--- src/public-api-service/src/main/java/com/azdaks/publicapiservice/config/AccountServiceConfiguration.java
+++ src/public-api-service/src/main/java/com/azdaks/publicapiservice/config/AccountServiceConfiguration.java
@@ -3,7 +3,9 @@ package com.azdaks.publicapiservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
@@ -18,6 +20,12 @@ public class AccountServiceConfiguration {
 
     @Bean
     public ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        /**
+         * This object mapper instance overrides the shared instance in CloudEvent class
+         * To avoid serialization issues with the CloudEvent class, we need to apply their ObjectMapper configuration
+         */
+        return new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 }
